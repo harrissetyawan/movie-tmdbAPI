cf7a2dc19eb342ff34622ac01ea5e7c459283cea
unknown <harrisfm90@gmail.com>
2018-12-18 19:49:14 +0700

    Index>Pagination/Belum Rapih

--- application/views/index.php
+++ application/views/index.php
@@ -63,8 +63,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <div class="row"> 
       <?= $hasil ?>
       </div>
+      
       <!-- Pagination -->
+      <ul class="pagination justify-content-center mt-5">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+<?= $page ?>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+
+      
     </div>
+
     <!-- /.container -->
 
     <!-- footer -->
